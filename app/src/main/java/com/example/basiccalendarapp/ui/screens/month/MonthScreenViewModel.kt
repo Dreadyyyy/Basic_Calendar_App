577fe9e7cfb23f183523f3cf932572e06f314515
Dreadyyyy <90562473+Dreadyyyy@users.noreply.github.com>
2024-03-09 21:48:45 +0100

    More functionality on month screen\nMonth screen now properly shows current month with a name on a top bar and highlighted current date. Upon clicking on a day card date is shown on the bottom half of the screen

--- app/src/main/java/com/example/basiccalendarapp/ui/screens/month/MonthScreenViewModel.kt
+++ app/src/main/java/com/example/basiccalendarapp/ui/screens/month/MonthScreenViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -13,22 +14,29 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.Month
 import java.util.Date
 
 class MonthScreenViewModel : ViewModel() {
-    private val _currentDate: Flow<LocalDate> = flow {
-        while (true) emit(LocalDate.now())
+    private var selectedDay: Int? = null
+    private val _currentDate: Flow<Pair<LocalDate, Int?>> = flow {
+        while (true) {
+            emit(LocalDate.now() to selectedDay)
+            delay(10L)
+        }
     }
-    val monthScreenUiState: StateFlow<MonthScreenUiState> =
-        _currentDate.map { date: LocalDate ->
+    var monthScreenUiState: StateFlow<MonthScreenUiState> =
+        _currentDate.map { (date: LocalDate, selectedDay: Int?) ->
             MonthScreenUiState(
                 monthName = date.month.name,
                 monthLength = date.month.length(date.isLeapYear),
-                currentDay = date.dayOfMonth
+                currentDay = date.dayOfMonth,
+                selectedDay = selectedDay
             )
         }.stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000L),
             initialValue = MonthScreenUiState()
         )
+    fun changeSelectedDay(newSelectedDay: Int) { selectedDay = newSelectedDay }
 }
